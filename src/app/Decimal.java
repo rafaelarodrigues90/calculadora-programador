755cd1d61cc720f00e9a39557546e32cf395e5f5
rafaelarodrigues90 <ltcmdrdata@protonmail.com>
2019-07-26 18:10:30 -0300

    bug fix: input voltando para Main quando recebe letra

--- src/app/Decimal.java
+++ src/app/Decimal.java
@@ -5,57 +5,64 @@ import java.util.Stack;
 
 public class Decimal {
 
-    final static int OCTAL = 8;
-    final static int BINARY = 2;
-
+    // pega número
     public static void mainDecimal() {
-
-        System.out.println("\n\n========== DECIMAL ==> BINÁRIO, OCTAL OU HEXADECIMAL =========\n");
-        
-        int numero, base;
+    
         Scanner scan = new Scanner(System.in);
-
-        do {
+        int numero;
+            
+        try {
+            System.out.println("\n\n========== DECIMAL ==> BINÁRIO, OCTAL OU HEXADECIMAL =========\n");
             System.out.println("\nDigite 0 para voltar ao menu.\nDigite -1 para terminar programa.\n\nDigite um número: ");
             numero = scan.nextInt();
 
             if (numero > 0) {
+                base(numero);
+            } else if (numero == 0) {
+                Main.main(null);
+            } else if (numero == -1) {
+                System.out.println("\nSaindo.\n");
+                System.exit(0);
+            } 
+        } catch(Exception e) {
+            mainDecimal();
+        }
+
+        scan.close();
+    }
+
+    final static int OCTAL = 8;
+    final static int BINARY = 2;
+
+    // pega base
+    public static void base(int numero) {
+        
+        Scanner scan = new Scanner(System.in);
+        int base;
+
+        try{        
+            do{
                 System.out.print("Digite 2 para binário ou 8 para octal: ");
                 base = scan.nextInt();
 
-                while (base != OCTAL && base != BINARY) {
-                    if (base == 0) {
-                        Main.main(null);
-                    } else {
-                        if (base == -1) {
-                            System.out.println("\nSaindo.\n");
-                            System.exit(0);
-                        }
-                    }
-                       
-                    System.out.print("Digite 2 para binário ou 8 para octal: ");
-                    base = scan.nextInt();
+                if(base == 0) {
+                    Main.main(null);
+                } else if (base == -1) {
+                    System.out.println("\nSaindo.\n");
+                    System.exit(0);
                 }
-
-                if (base == OCTAL){
-                    System.out.print("\nOctal ==>" + converterBinarioOctal(numero, base) + "\n______________________________________________________\n");
-                } else {
-                    System.out.print("\nBinário ==>" + converterBinarioOctal(numero, base) + "\n______________________________________________________\n");
-                }
-            }
-        } while (numero > 0);
-
-        if (numero == -1) {
-            System.out.println("\nSaindo.\n");
-            System.exit(0);
+                
+            } while(base != BINARY && base != OCTAL);
+        
+        } catch(Exception e) {
+            base(numero);
         }
-
-        Main.main(null);
+        
         scan.close();
     }
 
 
-    // binário e octal
+    // converte para binário ou octal
     public static String converterBinarioOctal (int numero, int base) {
         Stack<Integer> pilha = new Stack<Integer>();
         int resto;
--- src/app/Main.java
+++ src/app/Main.java
@@ -7,32 +7,38 @@ public class Main {
         
         Scanner scan = new Scanner(System.in);
         
-        System.out.println("\n\n========== CALCULADORA PROGRAMADOR =========\n");
-        System.out.println("1 - Decimal para binário, octal e hexadecimal");
-        System.out.println("2 - Binário para decimal, octal e hexadecimal");
-        System.out.println("3 - Octal para binário, decimal e hexadecimal");
-        System.out.println("3 - Hexadecimal para binário, decimal e octal");
-        System.out.println("0 - Sair\n");
-        
-        int key = scan.nextInt();
-        
-        switch (key) {
-            case 1:
-                Decimal.mainDecimal();
+        try{
+            System.out.println("\n\n========== CALCULADORA PROGRAMADOR =========\n");
+            System.out.println("1 - Decimal para binário, octal e hexadecimal");
+            System.out.println("2 - Binário para decimal, octal e hexadecimal");
+            System.out.println("3 - Octal para binário, decimal e hexadecimal");
+            System.out.println("3 - Hexadecimal para binário, decimal e octal");
+            System.out.println("0 - Sair\n");
+            
+            int key = scan.nextInt();
+            
+            switch (key) {
+                case 1:
+                    Decimal.mainDecimal();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 0:
+                    System.out.println("\nSaindo.\n");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("\nDigite uma das opções, por favor!");
+                    main(null);
                 break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 0:
-                System.out.println("\nSaindo.\n");
-                System.exit(0);
-                break;
-            default:
-                main(null);
-            break;
+            }
+        } catch (Exception e) {
+            System.out.println("\nDigite uma das opções, por favor!");
+            main(args);
         }
 
         scan.close();
